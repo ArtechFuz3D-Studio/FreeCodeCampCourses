c1cd8b6201040250d7a5e5d6d986fa71364612ac
ArtechFuz3D <72121107+clickonrefresh@users.noreply.github.com>
2024-07-18 07:53:46 +0200

    Part05

---
+++ JS/YTCourses/FrankLabsCourse/Part04/script.js
@@ -0,0 +1,100 @@
+/**
+ * @type {HTMLCanvasElement}
+ */
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+// Sizes
+const CANVAS_WIDTH = (canvas.width = 500);
+const CANVAS_HEIGHT = (canvas.height = 700);
+const canvasPosition = canvas.getBoundingClientRect();
+// console.log(canvasPosition)
+
+const explosions = [];
+
+class Explosion {
+  constructor(x, y) {
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    //division in JS is more expensive, instead of diving by 2, multiply by 0.5
+    this.width = this.spriteWidth * 0.6;
+    this.height = this.spriteHeight * 0.6;
+    this.x = x 
+    this.y = y 
+    this.image = new Image();
+    this.image.src = "../../../../public/boom.png";
+    this.frame = 0;
+    this.timer = 0;
+    this.angle = Math.random() * 6.2
+
+    //Audio
+    this.sound = new Audio()
+    this.sound2 = new Audio()
+    this.sound2.src = '../../../../public/roll4.wav'
+    // this.sound.src = '../../../../public/roll.wav'
+  }
+  update() {
+    // if (this.frame === 0) this.sound.play() && this.sound2.play()
+    if (this.frame === 0) this.sound2.play()
+    this.timer++;
+    if (this.timer % 10 === 0) {
+      this.frame++;
+    }
+    // this.frame++;
+  }
+  draw() {
+    ctx.save()
+    ctx.translate(this.x, this.y)
+    ctx.rotate(this.angle)
+    ctx.drawImage(
+      this.image,
+      this.spriteWidth * this.frame,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      0 - this.width * 0.5,
+      0 - this.height * 0.5,
+      this.width,
+      this.height
+    );
+    ctx.restore()
+  }
+}
+
+// window.addEventListener('click', function(e){
+//     // console.log(e)
+//     let positionX = e.x - canvasPosition.left
+//     let positionY = e.y - canvasPosition.top
+//     // ctx.fillStyle = 'white'
+//     // ctx.fillRect(e.x - canvasPosition.left -25, e.y - canvasPosition.top -25, 50, 50)
+//     explosions.push(new Explosion(positionX, positionY))
+//     console.log(explosions)
+// })
+
+window.addEventListener("click", function (e) {
+  createAnimation(e);
+});
+// window.addEventListener("mousemove", function (e) {
+//   createAnimation(e);
+// });
+
+function createAnimation(e) {
+  let positionX = e.x - canvasPosition.left;
+  let positionY = e.y - canvasPosition.top;
+  explosions.push(new Explosion(positionX, positionY));
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < explosions.length; i++) {
+    explosions[i].update();
+    explosions[i].draw();
+    if (explosions[i].frame > 5) {
+      explosions.splice(i, 1);
+      i--;
+    }
+  }
+  requestAnimationFrame(animate);
+}
+animate();
--- JS/YTCourses/FrankLabsCourse/README.md
+++ JS/YTCourses/FrankLabsCourse/README.md
@@ -3,5 +3,7 @@
 [Part 01 - Sprite Animation](/Part01/)
 [Part 02 - Parralax Background](/Part02/)
 [Part 03 - NPC Movement Animations](/Part03/)
+[Part 04 - Collisions, SFX, FX](/Part04/)
+[Part 05 - Game PointShooter](/Part05/)
 
 [Home](/FreeCodeCampCourses/#home)
