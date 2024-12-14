5112f4594a1f997a50132f1ac255071de185aab1
ArtechFuz3D-Studio <72121107+ArtechFuz3D-Studio@users.noreply.github.com>
2024-12-14 16:04:37 +0200

    up

--- src/JS/YTCourses/FCC/AIGame/car.js
+++ src/JS/YTCourses/FCC/AIGame/car.js
@@ -1,123 +1,132 @@
-
 import { Controls } from "./controls";
-import {Sensor} from './sensor'
+import { Sensor } from "./sensor";
 import { polyIntersect } from "./utils";
 
 export class Car {
-  constructor(x, y, width, height, roadBorders) {
+  constructor(x, y, width, height, controlType, maxSpeed = 10) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
 
     this.speed = 0;
-    this.acc = 0.2;
-    this.maxSpeed = 3;
-    this.freak = 0.02; // shin
+    this.acc = 0.5;
+    // this.maxSpeed = 3;
+    this.maxSpeed = maxSpeed;
+    this.freak = 0.09; // shin
     this.angle = 0;
-    this.damaged = false
+    this.damaged = false;
 
-    this.sensor = new Sensor(this)
-    // this.damaged = this.#assessDamage(roadBorders)
-    this.controls = new Controls();
-  }
-
-
-  update(roadBorders) {
-    if(!this.damaged){
-        this.#move()
-        this.polygon = this.#createPolygon()
-        this.damaged = this.#assessDamage(roadBorders)
+    if (controlType != "DUMMY") {
+      this.sensor = new Sensor(this);
     }
-  this.sensor.update(roadBorders)
+
+    // this.damaged = this.#assessDamage(roadBorders)
+    this.controls = new Controls(controlType);
   }
 
-  #assessDamage(roadBorders){
-    for (let i = 0; i < roadBorders.length; i++){
-      if(polyIntersect(this.polygon, roadBorders[i])){
-        return true
+  update(roadBorders, traffic) {
+    if (!this.damaged) {
+      this.#move();
+      this.polygon = this.#createPolygon();
+      this.damaged = this.#assessDamage(roadBorders, traffic);
+    }
+    if (this.sensor) {
+      this.sensor.update(roadBorders, traffic);
+    }
+  }
+
+  #assessDamage(roadBorders, traffic) {
+    for (let i = 0; i < roadBorders.length; i++) {
+      if (polyIntersect(this.polygon, roadBorders[i])) {
+        return true;
       }
     }
-    return false
+    for (let i = 0; i < traffic.length; i++) {
+      if (polyIntersect(this.polygon, traffic[i].polygon)) {
+        return true;
+      }
+    }
+    return false;
   }
 
-
-  #createPolygon(){
-    const points = []
-    const rad = Math.hypot(this.width, this.height)*0.5
-    const alpha = Math.atan2(this.width, this.height)
+  #createPolygon() {
+    const points = [];
+    const rad = Math.hypot(this.width, this.height) * 0.5;
+    const alpha = Math.atan2(this.width, this.height);
     points.push({
       x: this.x - Math.sin(this.angle - alpha) * rad,
-      y: this.y - Math.cos(this.angle - alpha)* rad
-    })
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    });
     points.push({
       x: this.x - Math.sin(this.angle + alpha) * rad,
-      y: this.y - Math.cos(this.angle + alpha)* rad
-    })
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    });
     points.push({
       x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
-      y: this.y - Math.cos(Math.PI + this.angle - alpha)* rad
-    })
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+    });
     points.push({
       x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
-      y: this.y - Math.cos(Math.PI + this.angle + alpha)* rad
-    })
-    return points
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+    });
+    return points;
   }
 
-  #move(){
+  #move() {
     if (this.controls.forward) {
-        this.speed += this.acc;
+      this.speed += this.acc;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acc;
+    }
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed * 0.5) {
+      this.speed = -this.maxSpeed * 0.5;
+    }
+    if (this.speed > 0) {
+      this.speed -= this.freak;
+    }
+    if (this.speed < 0) {
+      this.speed += this.freak;
+    }
+    if (Math.abs(this.speed) < this.freak) {
+      this.speed = 0;
+    }
+    // flip for reverse
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      // Left
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
       }
-      if (this.controls.reverse) {
-        this.speed -= this.acc;
+      // Right
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
       }
-      if (this.speed > this.maxSpeed) {
-        this.speed = this.maxSpeed;
-      }
-      if (this.speed < -this.maxSpeed * 0.5) {
-        this.speed = -this.maxSpeed * 0.5;
-      }
-      if (this.speed > 0) {
-        this.speed -= this.freak;
-      }
-      if (this.speed < 0) {
-        this.speed += this.freak;
-      }
-      if (Math.abs(this.speed) < this.freak) {
-        this.speed = 0;
-      }
-      // flip for reverse
-      if (this.speed != 0) {
-        const flip = this.speed > 0 ? 1 : -1;
-        // Left
-        if (this.controls.left) {
-          this.angle += 0.03*flip
-        }
-        // Right
-        if (this.controls.right) {
-          this.angle -= 0.03*flip
-        }
-      }
-  
-      this.x -= Math.sin(this.angle) * this.speed;
-      this.y -= Math.cos(this.angle) * this.speed;
+    }
 
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx) {
-    if(this.damaged){
-      ctx.fillStyle = "orange"
-    } else{
-      ctx.fillStyle= "black"
+  draw(ctx, color) {
+    if (this.damaged) {
+      ctx.fillStyle = "snow";
+    } else {
+      ctx.fillStyle = color;
     }
-    ctx.beginPath()
-    ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
-    for (let i = 1; i < this.polygon.length; i++){
-      ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+    ctx.beginPath();
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
     }
-    ctx.fill()
+    ctx.fill();
 
-    this.sensor.draw(ctx)
+    if (this.sensor) {
+      this.sensor.draw(ctx);
+    }
   }
 }
--- src/JS/YTCourses/FCC/AIGame/main.js
+++ src/JS/YTCourses/FCC/AIGame/main.js
@@ -7,23 +7,31 @@ canvas.width = 200
 
 const ctx = canvas.getContext("2d")
 const road=new Road(canvas.width*0.5, canvas.width*0.9)
-const car=new Car(road.getLaneCenter(2), 100, 30, 50)
+const car=new Car(road.getLaneCenter(2), 100, 30, 50, "KEYS")
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 3)]
 // car.draw(ctx)
 
 animate()
 function animate(){
 
+    for(let i = 0; i < traffic.length; i++){
+        traffic[i].update(road.borders,[])
+    }
     // ctx.clearRect(0,0,canvas.width,canvas.height)
-    car.update(road.borders)
+    car.update(road.borders, traffic)
     canvas.height=window.innerHeight
 
     ctx.save()
 
     ctx.translate(0, -car.y+canvas.height*0.7)
     road.draw(ctx)
-    car.draw(ctx)
+    for(let i = 0; i < traffic.length; i++){
+        traffic[i].draw(ctx, "mediumvioletred")
+    }
+    car.draw(ctx, "mediumspringgreen")
 
     ctx.restore()
 
     requestAnimationFrame(animate)
 }
+
