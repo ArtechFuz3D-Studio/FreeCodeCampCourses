c4ef0360b07e896e07771371174fb22ad6bc1576
ArtechFuz3D-Studio <72121107+ArtechFuz3D-Studio@users.noreply.github.com>
2024-12-19 06:22:35 +0200

    p2-3

---
+++ src/JS/YTCourses/FCC/AIGameP2_3/helpers/controls.js
@@ -0,0 +1,76 @@
+import { Point } from "./primitives/point";
+import { Segment } from "./primitives/segment";
+
+
+
+myCanvas.width = 600;
+myCanvas.height = 600;
+
+const ctx = myCanvas.getContext("2d");
+
+export function removeAll(){
+  document.getElementById("remall").addEventListener("click", () => {
+  graph.dispose()
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+  graph.draw(ctx);
+});
+}
+
+export function removeRandomSegment(graph, myCanvas, ctx){
+  document.getElementById("remsegment").addEventListener("click", () => {
+  if (graph.segments.length == 0) {
+    console.log('none segments left to remove')
+    return
+  }
+  const index = Math.floor(Math.random()  * graph.segments.length)
+  graph.removeSegment(graph.segments[index])
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+  graph.draw(ctx);
+});
+}
+
+export function removeRandomPoint(graph, myCanvas, ctx){
+  document.getElementById("rempoint").addEventListener("click", () => {
+  if (graph.points.length == 0) {
+    console.log('none points left to remove')
+    return
+  }
+  const index = Math.floor(Math.random()  * graph.points.length)
+  graph.removePoint(graph.points[index])
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+  graph.draw(ctx);
+});
+}
+
+export function addRandomSegment(graph, myCanvas, ctx) {
+  document.getElementById("addsegment").addEventListener("click", () => {
+   
+    const index1 = Math.floor(Math.random() * graph.points.length)
+    const index2 = Math.floor(Math.random() * graph.points.length)
+    let success = false
+    if (index1 != index2) {
+      success = graph.tryAddSegment(
+            new Segment(graph.points[index1], graph.points[index2])
+        )
+    }
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    graph.draw(ctx);
+    // console.log(success);
+
+  });
+}
+
+export function addRandomPoint(graph) {
+  document.getElementById("addpoint").addEventListener("click", () => {
+    const success = graph.tryAddPoint(
+      new Point(
+        Math.random() * myCanvas.width, 
+        Math.random() * myCanvas.height
+    )
+    );
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    graph.draw(ctx);
+    // console.log(success);
+  });
+}
+
---
+++ src/JS/YTCourses/FCC/AIGameP2_3/main.js
@@ -0,0 +1,94 @@
+import { Point } from "./helpers/primitives/point.js"
+import { Graph } from "./helpers/math/graph.js"
+import { Segment } from "./helpers/primitives/segment.js"
+import { addRandomPoint, addRandomSegment, removeRandomSegment, removeRandomPoint, removeAll } from "./helpers/controls.js";
+import { GraphEditor } from "./helpers/graphEditor.js";
+import { Viewport } from "./helpers/viewport.js";
+
+
+// myCanvas.width = 600
+// myCanvas.height = 600
+
+const ctx = myCanvas.getContext("2d")
+
+// removed after adding save function
+// const p1 = new Point(200, 200)
+// const p2 = new Point(500, 200)
+// const p3 = new Point(400, 400)
+// const p4 = new Point(100, 300)
+
+// const s1 = new Segment(p1, p2)
+// const s2 = new Segment(p1, p3)
+// const s3 = new Segment(p1, p4)
+// const s4 = new Segment(p2, p3)
+// const s5 = new Segment(p4, p3)
+
+const graphString = localStorage.getItem('graph')
+const graphInfo = graphString ? JSON.parse(graphString) : null
+// const graph = new Graph([p1, p2, p3, p4], [s1, s2, s3, s4, s5])
+const graph = graphInfo
+    ? Graph.load(graphInfo)
+    :  new Graph()
+const viewport = new Viewport(myCanvas)
+// const graphEditor = new GraphEditor(myCanvas, graph,)
+const graphEditor = new GraphEditor(viewport, graph,)
+// graph.draw(ctx)
+
+animate()
+function animate(){
+    // ctx.restore()
+    // ctx.clearRect(0,0,myCanvas.width, myCanvas.height)
+    // ctx.save()
+    // ctx.translate(viewport.center.x, viewport.center.y)
+    // ctx.scale(1 / viewport.zoom, 1 / viewport.zoom)
+    // const offset = viewport.getOffset()
+    // ctx.translate(offset.x, offset.y)
+
+    viewport.reset()
+    graphEditor.display()
+
+
+    requestAnimationFrame(animate)
+}
+
+export function dispose() {
+    document.getElementById("dispose").addEventListener("click", () => {
+    graphEditor.dispose()
+}
+)}
+export function save(graph){
+    document.getElementById("save").addEventListener("click", () => {
+        localStorage.setItem("graph", JSON.stringify(graph))
+    })
+
+}
+//
+// Expose graph to the global scope for debugging
+window.graph = graph; // or globalThis.graph = graph
+// or
+// console.log(graph); // Prints the graph instance to the browser console
+
+// Expose variables globally for debugging
+// window.p1 = p1;
+// window.p2 = p2;
+// window.p3 = p3;
+// window.p4 = p4;
+// window.graph = graph;
+window.graphEditor = graphEditor;
+window.viewport = viewport;
+// window.graphInfo = graphInfo
+
+// Attach to the global scope
+window.dispose = dispose;
+window.save = save;
+
+
+//
+// Set up the event listener to add random points
+addRandomPoint(graph, ctx, myCanvas);
+addRandomSegment(graph, myCanvas, ctx)
+removeRandomSegment(graph, myCanvas, ctx)
+removeRandomPoint(graph, myCanvas, ctx)
+removeAll(graph, myCanvas, ctx)
+save(graph, myCanvas, ctx)
+dispose(graph, myCanvas, ctx)
